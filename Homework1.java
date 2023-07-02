055d54bccafb542a46ff27c627e1a323e31ff69f
MarinaNikole <126466977+MarinaNikole@users.noreply.github.com>
2023-07-02 16:54:03 +0200

    Homework_27_06_23

---
+++ Homework1.java
@@ -0,0 +1,49 @@
+package task3;
+
+public class Homework1 {
+    /*
+                        Какова временная сложность?
+
+task#1      O(n)
+
+    START
+    READ number n
+    IF n==1 THEN return
+    FOR i=1, i<=n, i+1
+    print "*"
+    END
+===========================================================================
+
+task#2      O(n log n)
+    START
+    READ number n
+    numbers i=0, j=0, a=0
+    FOR i=n/2, i<n; i+1
+    FOR j=2, j<n, j*2
+    a=a+n/2
+    END
+========================================================================
+
+task#3         O(n^2)
+    START
+   READ number n
+   number a=0
+   FOR i=0, i<n, i+1
+   FOR j=n, j>i, j-1
+   a=a+i+j
+   END
+
+========================================================================
+
+task#4      O(log n)
+   START
+   READ number n
+   numbers a=0, i=n
+   WHILE i>0
+   a=a+i
+   i= i/2
+   END
+
+
+     */
+}
